f9aecfb7cb10c0367ba08cae98a9dabaee419cb3
arseniy <r@ex.com>
2023-12-12 10:34:24 +0300

    feat: remove scroll while burger menu disabled

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d715f496319cb8ed"; }
+/******/ 		__webpack_require__.h = function() { return "5e03811bde8d1d3d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
